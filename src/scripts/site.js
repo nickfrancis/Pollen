777105af34c4dd46ad4338e17a0ef6944c194eeb
nfrancis81 <nick@nick-francis.org>
2020-05-29 23:02:12 +0100

    no message

--- src/scripts/site.js
+++ src/scripts/site.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
   // Anything you want to run once the page has loaded goes here... remove the // from the line below to check the JavaScript is reloading as expected
   // alert('hello world');
-});
+
+  var settings = {
+  	"async": true,
+  	"crossDomain": true,
+  	"url": "https://accuweatherstefan-skliarovv1.p.rapidapi.com/get24HoursConditionsByLocationKey",
+  	"method": "POST",
+  	"headers": {
+  		"x-rapidapi-host": "AccuWeatherstefan-skliarovV1.p.rapidapi.com",
+  		"x-rapidapi-key": "03cea27b90msh979169fdef0e7f5p1a90cfjsn7cc16810642b",
+  		"content-type": "application/x-www-form-urlencoded"
+  	},
+  	"data": {
+  		"apiKey": "A3nqqGOEvaRJUnDv6fAbFVZDBBNTd2DS",
+  		"locationKey": "Thame, UK"
+  	}
+  }
+
+  $.ajax(settings).done(function (response) {
+  	console.log(response);
+  });
